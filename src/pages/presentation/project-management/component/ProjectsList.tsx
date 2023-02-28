ec8e4e97394e7b104136a86d07ab497a3394b936
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-02-28 12:22:41 +0000

    Report form moved to reports list

--- src/pages/presentation/project-management/component/ProjectsList.tsx
+++ src/pages/presentation/project-management/component/ProjectsList.tsx
@@ -20,12 +20,6 @@ import useSortableData from '../../../../hooks/useSortableData';
 import useDarkMode from '../../../../hooks/useDarkMode';
 import { useNavigate } from 'react-router-dom';
 import { APP_PATHS } from '../../../../routes/contentRoutes';
-import classNames from 'classnames';
-import OffCanvas, { OffCanvasBody, OffCanvasHeader, OffCanvasTitle } from '../../../../components/bootstrap/OffCanvas';
-import FormGroup from '../../../../components/bootstrap/forms/FormGroup';
-import { FormikHelpers, useFormik } from 'formik';
-import moment from 'moment';
-import Input from '../../../../components/bootstrap/forms/Input';
 
 interface ICommonUpcomingEventsProps {
 	isFluid?: boolean;
@@ -43,33 +37,6 @@ const ProjectLists: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 		navigate(`../${APP_PATHS.PROJECTS.ITEM}/${id}`);
 	};
 
-	const [projectEditOffcanvas, setProjectEditOffcanvas] = useState(false);
-	const [editModeOffCanvas, setEditModeOffCanvas] = useState(false);
-	const handleProjectAdd = () => {
-		setProjectEditOffcanvas(!projectEditOffcanvas);
-		setEditModeOffCanvas(false);
-	};
-	const handleProjectEdit = () => {
-		setProjectEditOffcanvas(!projectEditOffcanvas);
-		setEditModeOffCanvas(true);
-	};
-
-	const formik = useFormik({
-		onSubmit<Values>(
-			values: Values,
-			formikHelpers: FormikHelpers<Values>,
-		): void | Promise<any> {
-			return undefined;
-		},
-		initialValues: {
-			project: 'Project D',
-			startedDate: moment().add(1, 'days').format('YYYY-MM-DD'),
-			endDate: moment().add(90, 'days').format('YYYY-MM-DD'),
-			teamMembers: '',
-			status: 'Green'
-		},
-	});
-
 	return (
 		<>
 			<Card stretch={isFluid}>
@@ -79,13 +46,14 @@ const ProjectLists: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 					</CardLabel>
 					<CardActions>
 						<Button
-							onClick={handleProjectAdd}
 							color='info'
-							icon='Add'
+							icon='CloudDownload'
 							isLight
 							tag='a'
+							to='/somefile.txt'
+							target='_blank'
 							download>
-							Add Project Report
+							Export
 						</Button>
 					</CardActions>
 				</CardHeader>
@@ -95,45 +63,20 @@ const ProjectLists: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 							<tr>
 								<td style={{ width: 60 }} />
 								<th>Project</th>
-								<th>Start Date</th>
+								<th>Started Date</th>
 								<th>End Date</th>
-								<th>Team Members</th>
-								<th>RAG Status</th>
-								<th></th>
+								<th>Team Memebers</th>
+								<th>Status</th>
 								<td />
 							</tr>
 						</thead>
 						<tbody>
 							{dataPagination(items, currentPage, perPage).map((item) => (
-								<tr key={item.id} className='navigation-item cursor-pointer'>
-									<td>
-										<Button
-											isOutline={!darkModeStatus}
-											color='dark'
-											isLight={darkModeStatus}
-											className={classNames({
-												'border-light': !darkModeStatus,
-											})}
-											icon='Info'
-											onClick={()=> handleOnClick(item.id)}
-											aria-label='Detailed information'
-										/>
-									</td>
+								<tr key={item.id} onClick={()=> handleOnClick(item.id)} className='navigation-item cursor-pointer'>
+									<td></td>
 									<td>{item.name}</td>
-									<td>
-										<span className='text-nowrap'>
-											{moment(item.startedDate).format(
-												'MMM Do YYYY',
-											)}
-										</span>
-									</td>
-									<td>
-										<span className='text-nowrap'>
-											{moment(item.endDate).format(
-												'MMM Do YYYY',
-											)}
-										</span>
-									</td>
+									<td>{item.startedDate}</td>
+									<td>{item.endDate}</td>
 									<td>{item.teamMembers}</td>
 									<td>
 										<Dropdown>
@@ -164,19 +107,6 @@ const ProjectLists: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 									<td>
 	
 									</td>
-									<td>
-										<Button
-											isOutline={!darkModeStatus}
-											color='dark'
-											isLight={darkModeStatus}
-											className={classNames('text-nowrap', {
-												'border-light': !darkModeStatus,
-											})}
-											icon='Edit'
-											onClick={handleProjectEdit}>
-											Edit
-										</Button>
-									</td>
 								</tr>
 							))}
 						</tbody>
@@ -191,73 +121,6 @@ const ProjectLists: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 					setPerPage={setPerPage}
 				/>
 			</Card>
-
-			<OffCanvas
-				setOpen={setProjectEditOffcanvas}
-				isOpen={projectEditOffcanvas}
-				titleId='projectAddEdit'
-				isBodyScroll
-				placement='end'>
-				<OffCanvasHeader setOpen={setProjectEditOffcanvas}>
-					<OffCanvasTitle id='projectAddEdit'>Add Project Report</OffCanvasTitle>
-				</OffCanvasHeader>
-				<OffCanvasBody>
-					<div className='row g-4'>
-						<div className='col-12'>
-							<FormGroup id='project' label='Project'>
-								<Input
-									onChange={formik.handleChange}
-									value={formik.values.project}
-								/>
-							</FormGroup>
-						</div>
-						<div className='col-12'>
-							<FormGroup id='startedDate' label='Started Date'>
-								<Input
-									onChange={formik.handleChange}
-									value={formik.values.startedDate}
-									type='date'
-								/>
-							</FormGroup>
-						</div>
-						<div className='col-12'>
-							<FormGroup id='endDate' label='End Date'>
-								<Input
-									onChange={formik.handleChange}
-									value={formik.values.endDate}
-									type='date'
-								/>
-							</FormGroup>
-						</div>
-						<div className='col-12'>
-							<FormGroup id='teamMembers' label='Team Members'>
-								<Input
-									onChange={formik.handleChange}
-									value={formik.values.teamMembers}
-								/>
-							</FormGroup>
-						</div>
-						<div className='col-6'>
-							<FormGroup id='status' label='RAG Status'>
-								<Input
-									onChange={formik.handleChange}
-									value={formik.values.status}
-								/>
-							</FormGroup>
-						</div>
-					</div>
-				</OffCanvasBody>
-				<div className='row m-0'>
-					<div className='col-12 p-3'>
-						<Button
-							color='info'
-							className='w-100'
-							onClick={() => setProjectEditOffcanvas(false)}>
-							Save
-						</Button>
-					</div>
-				</div>
-			</OffCanvas>
 		</>
 	);
 };
--- src/pages/presentation/project-management/component/ReportsList.tsx
+++ src/pages/presentation/project-management/component/ReportsList.tsx
@@ -13,6 +13,13 @@ import PaginationButtons, { dataPagination, PER_COUNT } from '../../../../compon
 import useSortableData from '../../../../hooks/useSortableData';
 import useDarkMode from '../../../../hooks/useDarkMode';
 import { useParams } from 'react-router-dom';
+import OffCanvas, { OffCanvasBody, OffCanvasHeader, OffCanvasTitle } from '../../../../components/bootstrap/OffCanvas';
+import FormGroup from '../../../../components/bootstrap/forms/FormGroup';
+import Input from '../../../../components/bootstrap/forms/Input';
+import { FormikHelpers, useFormik } from 'formik';
+import moment from 'moment';
+import classNames from 'classnames';
+
 
 interface ICommonUpcomingEventsProps {
 	isFluid?: boolean;
@@ -25,6 +32,34 @@ const CommonUpcomingEvents: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 	const [perPage, setPerPage] = useState(PER_COUNT['5']);
 	const { items } = useSortableData(getReportDataByProjectId(id));
 
+	const formik = useFormik({
+		onSubmit<Values>(
+			values: Values,
+			formikHelpers: FormikHelpers<Values>,
+		): void | Promise<any> {
+			return undefined;
+		},
+		initialValues: {
+			project: 'Project D',
+			startedDate: moment().add(1, 'days').format('YYYY-MM-DD'),
+			endDate: moment().add(90, 'days').format('YYYY-MM-DD'),
+			teamMembers: '',
+			status: 'Green'
+		},
+	});
+
+	const handleProjectAdd = () => {
+		setProjectEditOffcanvas(!projectEditOffcanvas);
+		setEditModeOffCanvas(false);
+	};
+	const handleProjectEdit = () => {
+		setProjectEditOffcanvas(!projectEditOffcanvas);
+		setEditModeOffCanvas(true);
+	};
+
+	const [projectEditOffcanvas, setProjectEditOffcanvas] = useState(false);
+	const [editModeOffCanvas, setEditModeOffCanvas] = useState(false);
+
 	return (
 		<>
 			<Card stretch={isFluid}>
@@ -34,14 +69,13 @@ const CommonUpcomingEvents: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 					</CardLabel>
 					<CardActions>
 						<Button
+							onClick={handleProjectAdd}
 							color='info'
-							icon='CloudDownload'
+							icon='Add'
 							isLight
 							tag='a'
-							to='/somefile.txt'
-							target='_blank'
 							download>
-							Export
+							Add Project Report
 						</Button>
 					</CardActions>
 				</CardHeader>
@@ -64,7 +98,17 @@ const CommonUpcomingEvents: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 									<td>{item.enteredBy}</td>
 									<td>{item.description}</td>
 									<td>
-	
+									<Button
+											isOutline={!darkModeStatus}
+											color='dark'
+											isLight={darkModeStatus}
+											className={classNames('text-nowrap', {
+												'border-light': !darkModeStatus,
+											})}
+											icon='Edit'
+											onClick={handleProjectEdit}>
+											Edit
+										</Button>
 									</td>
 								</tr>
 							))}
@@ -81,7 +125,72 @@ const CommonUpcomingEvents: FC<ICommonUpcomingEventsProps> = ({ isFluid }) => {
 				/>
 			</Card>
 
-
+			<OffCanvas
+				setOpen={setProjectEditOffcanvas}
+				isOpen={projectEditOffcanvas}
+				titleId='projectAddEdit'
+				isBodyScroll
+				placement='end'>
+				<OffCanvasHeader setOpen={setProjectEditOffcanvas}>
+					<OffCanvasTitle id='projectAddEdit'>Add Project Report</OffCanvasTitle>
+				</OffCanvasHeader>
+				<OffCanvasBody>
+					<div className='row g-4'>
+						<div className='col-12'>
+							<FormGroup id='project' label='Project'>
+								<Input
+									onChange={formik.handleChange}
+									value={formik.values.project}
+								/>
+							</FormGroup>
+						</div>
+						<div className='col-12'>
+							<FormGroup id='startedDate' label='Started Date'>
+								<Input
+									onChange={formik.handleChange}
+									value={formik.values.startedDate}
+									type='date'
+								/>
+							</FormGroup>
+						</div>
+						<div className='col-12'>
+							<FormGroup id='endDate' label='End Date'>
+								<Input
+									onChange={formik.handleChange}
+									value={formik.values.endDate}
+									type='date'
+								/>
+							</FormGroup>
+						</div>
+						<div className='col-12'>
+							<FormGroup id='teamMembers' label='Team Members'>
+								<Input
+									onChange={formik.handleChange}
+									value={formik.values.teamMembers}
+								/>
+							</FormGroup>
+						</div>
+						<div className='col-6'>
+							<FormGroup id='status' label='RAG Status'>
+								<Input
+									onChange={formik.handleChange}
+									value={formik.values.status}
+								/>
+							</FormGroup>
+						</div>
+					</div>
+				</OffCanvasBody>
+				<div className='row m-0'>
+					<div className='col-12 p-3'>
+						<Button
+							color='info'
+							className='w-100'
+							onClick={() => setProjectEditOffcanvas(false)}>
+							Save
+						</Button>
+					</div>
+				</div>
+			</OffCanvas>
 		</>
 	);
 };
