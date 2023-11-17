0ecf93d38bc3b215ab5b1afa908918c9f697ab7a
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-17 15:38:42 +0000

    Turned on real data for projects

--- src/common/data/dummyFeatureFlagsData.ts
+++ src/common/data/dummyFeatureFlagsData.ts
@@ -29,7 +29,7 @@ const data: IFeatureFlagGroup[] = [
 		groupName :"Projects",
 		description : "FeatureFlags related to Projects",
 		flags : [
-            {flagName:"UseDummyData", isActive: true}
+            {flagName:"UseDummyData", isActive: false}
         ]
 	},
 	{
