81284804d0ed2a45b52e4892e074cb44fea78835
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-17 14:30:35 +0000

    Project Service

--- src/contexts/dataContext/projectService.tsx
+++ src/contexts/dataContext/projectService.tsx
@@ -3,10 +3,12 @@ import data from '../../common/data/dummyProjectsData';
 import FeatureFlagsContext from './../../contexts/featureFlagsContext';
 import { useContext } from 'react';
 import RAG_STATUS, { IStatus } from '../../models/ui-models/enums/enumStatus';
+import { GetAppSettings } from '../../appsettings';
+import { IProjectDto } from '../../models/dtos/IProjectDto';
 
 export interface IProjectService {
-	getProjectsByClientId(id?: string): Promise<IProjectModel[]>;
-	getProjectById(id?: string): Promise<IProjectModel | undefined>;
+	getProjectsByClientId(id?: string): Promise<IProjectDto[]>;
+	getProjectById(id?: string): Promise<IProjectDto>;
 }
 
 class ProjectService implements IProjectService {
@@ -14,35 +16,35 @@ class ProjectService implements IProjectService {
     useDummyData: boolean = false;
 
     constructor() {
-        //this.apiEndpoint = GetAppSettings().ProjectsApiUrl;            Just using Dummy Data until endpoints available
+        this.apiEndpoint = GetAppSettings().ProjectsApiUrl;
         const { featureFlags } = useContext(FeatureFlagsContext);
         this.useDummyData = featureFlags.isFeatureActive("Projects","UseDummyData");
     }
 
-    getProjectsByClientId(id?: string): Promise<IProjectModel[]> {
+    getProjectsByClientId(id?: string): Promise<IProjectDto[]> {
 
         if(this.useDummyData){
             return Promise.resolve( data);
         }
         
         //  Real implementation not available yet
-        return Promise.resolve([]);
+        var path = `${this.apiEndpoint}api/projects/${id}`;
+
+        return fetch(path)
+            // the JSON body is taken from the response
+            .then(response => response.json())
+            .then(json => {
+                return json as IProjectDto[];
+            });
     }
 
-    getProjectById(id?: string): Promise<IProjectModel | undefined> {
+    getProjectById(id?: string): Promise<IProjectDto> {
         if(this.useDummyData){
-            return Promise.resolve( data.find(x=>x.id == id));
+            return Promise.resolve( data[0]);
         }
         
-        //  Real implementation not available yet
-        return Promise.resolve({id: "-1",
-            name: '',
-            startedDate: '',
-            endDate: '',
-            teamMembers: [],
-            status: RAG_STATUS.APPROVED,
-            description: '',
-            image: ''});
+        //todo real implementation
+        return Promise.resolve( data[0]);
     }
 
 }
